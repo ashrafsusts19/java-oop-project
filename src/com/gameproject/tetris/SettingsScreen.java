52323532fcc0c2db245b6a8a2f7a80b85cd05f16
sayem000 <105208870+sayem000@users.noreply.github.com>
2022-08-15 23:51:41 +0600

    Basic code for Record and Settings Screen added

--- src/com/gameproject/tetris/SettingsScreen.java
+++ src/com/gameproject/tetris/SettingsScreen.java
@@ -5,18 +5,26 @@ import java.awt.*;
 
 public class SettingsScreen extends JPanel implements GameStage{
 
-    GameFrame mainGame;
-    SettingsScreen(GameFrame _mainGame){
-        mainGame = _mainGame;
+    GameFrame _mainGame;
+    SettingsScreen(GameFrame __mainGame ){
+        _mainGame=__mainGame;
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.decode("#3f468f"));
+        this.setFocusable(true);
     }
 
+    public void paintComponent(Graphics gg){
+        super.paintComponent(gg);
+    }
     @Override
     public void gameLoop() {
-
+        this.repaint();
     }
 
     @Override
     public void keyPressed(String key) {
+        switch(key){
 
+        }
     }
 }
